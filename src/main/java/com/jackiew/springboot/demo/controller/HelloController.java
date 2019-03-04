443343c193fbad082f9a985e9ec21e3ac776018f
jackiew <jackiew@synnex.com>
2019-03-04 11:16:26 +0800

    modify the test controller to save more session attribute

--- src/main/java/com/jackiew/springboot/demo/controller/HelloController.java
+++ src/main/java/com/jackiew/springboot/demo/controller/HelloController.java
@@ -13,7 +13,9 @@ public class HelloController {
     @RequestMapping(method = RequestMethod.GET)
     public String hello(HttpServletRequest request) {
         final HttpSession session = request.getSession();
-        session.setAttribute("someAttribute", "someAttributeValue");
+        session.setAttribute("someAttribute1", "111");
+        session.setAttribute("someAttribute2", "222");
+        session.setAttribute("someAttribute3", "333");
         String sessionId = session.getId();
         return "hello your session id:" + sessionId;
     }
